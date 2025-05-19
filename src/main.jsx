1da15c603e7dff4d6aa587471747078f0010dec6
omotayobolu <omotayodaniel10@gmail.com>
2025-05-19 15:51:24 +0100

    feat: added msw to root file

---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+
+async function enableMocking() {
+  if (import.meta.env.MODE !== "development") {
+    return;
+  }
+
+  const { worker } = await import("./mocks/browser.js");
+  return worker.start();
+}
+
+enableMocking().then(() => {
+  createRoot(document.getElementById("root")).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+});
